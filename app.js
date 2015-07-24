5040f644dcb4ab57817eedc4129c3a0809bca0d9
Steve Domenico <stratcat944@gmail.com>
2015-07-24 09:39:19 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var indexController = require('./controllers/index.js');
+
+var app = express();
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', indexController.index);
+console.log(process.env);
+
+var port = process.env.PORT || 4381;
+
+var server = app.listen(port, function() {
+	console.log('Express server listening on port ' + server.address().port);
+});
